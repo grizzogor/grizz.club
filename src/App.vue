783df8057a78a7d2f3e83dd606fc5d2f9a2c3ad6
Nils Drescher <nidresch@googlemail.com>
2024-05-04 14:54:06 +0200

    vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <SiteHeader></SiteHeader>
+</template>
+
+<script>
+import SiteHeader from '@/components/SiteHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+
+}
+
+body {
+  background-image: url('./assets/img/bg.gif');
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="_siteHeader">
+        <img src="@/assets/img/header/logo.png" /><img src="@/assets/img/header/header.gif" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SiteHeader',
+}
+</script>
+
+<style scoped>
+._siteHeader>img {
+    vertical-align: middle;
+}
+</style>
